d2b890b908b9c7995c261345a3b7dcae4593d9f3
mohd-adeeb011 <110835808+mohd-adeeb011@users.noreply.github.com>
2022-08-08 19:58:11 +0530

    Add files via upload

---
+++ 1_InstallingR.R
@@ -0,0 +1,23 @@
+# File:   InstallingR.R
+
+# LOAD DATA ################################################
+
+library(datasets)  # Load built-in datasets
+
+# SUMMARIZE DATA ###########################################
+
+head(iris)         # Show the first six lines of iris data
+summary(iris)      # Summary statistics for iris data
+plot(iris)         # Scatterplot matrix for iris data
+
+# CLEAN UP #################################################
+
+# Clear packages
+detach("package:datasets", unload = TRUE)  # For base #to unload packages
+
+# Clear plots
+dev.off()  # But only if there IS a plot
+
+# Clear console
+
+# Clear mind :)
